ae8d7d3861a1d5d3f39a511bd4311a07161ccf20
MartinFlischman <mflischman@icloud.com>
2022-08-26 11:47:36 +0200

    Buttons
    - "Start Game" button was added that starts a new game and resets all scores when pressed/clicked,
    - The add/subtract buttons were styled.

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
-let homeScoreEl = document.getElementById("home-score")
-let guestScoreEl = document.getElementById("guest-score")
-let homeScore = 0
-let guestScore = 0
+let homeScoreEl = document.getElementById("home-score");
+let guestScoreEl = document.getElementById("guest-score");
+let homeScore = 0;
+let guestScore = 0;
 
+// Home Score Add
 function addHome1() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore
+	homeScore += 1;
+	homeScoreEl.textContent = homeScore;
 }
 
 function addHome2() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
+	homeScore += 2;
+	homeScoreEl.textContent = homeScore;
 }
 
 function addHome3() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
+	homeScore += 3;
+	homeScoreEl.textContent = homeScore;
 }
 
+// Home Score Subtract
+function subtractHome1() {
+	homeScore -= 1;
+	homeScoreEl.textContent = homeScore;
+}
+
+function subtractHome2() {
+	homeScore -= 2;
+	homeScoreEl.textContent = homeScore;
+}
+
+function subtractHome3() {
+	homeScore -= 3;
+	homeScoreEl.textContent = homeScore;
+}
+
+// Guest Score Add
 function addGuest1() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore
+	guestScore += 1;
+	guestScoreEl.textContent = guestScore;
 }
 
 function addGuest2() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
+	guestScore += 2;
+	guestScoreEl.textContent = guestScore;
 }
 
 function addGuest3() {
-    guestScore += 3
-    guestScoreEl.textContent = guestScore
+	guestScore += 3;
+	guestScoreEl.textContent = guestScore;
+}
+
+// Guest Score Subtract
+function subtractGuest1() {
+	guestScore -= 1;
+	guestScoreEl.textContent = guestScore;
+}
+
+function subtractGuest2() {
+	guestScore -= 2;
+	guestScoreEl.textContent = guestScore;
+}
+
+function subtractGuest3() {
+	guestScore -= 3;
+	guestScoreEl.textContent = guestScore;
+}
+
+function startGame() {
+	homeScore = 0;
+	homeScoreEl.textContent = homeScore;
+	guestScore = 0;
+	guestScoreEl.textContent = guestScore;
 }
